ecbd7f36316541e36d3e7eee21a26ec6311d8588
Randy Davis <randy_davis@outlook.com>
2014-12-15 22:58:49 -0600

    add bootstrap, jQuery, removed old partials, and update nav bar and controller.js file

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,11 +4,6 @@
 
 angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function() {
-    
-  }])
-  .controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
 
   }]);
+  
