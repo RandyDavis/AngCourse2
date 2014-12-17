6cade2af0a013af549dd087120edf8e3a2c54250
Randy Davis <randy_davis@outlook.com>
2014-12-16 23:05:49 -0600

    setup code to send text messages

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,15 +7,31 @@ angular.module('myApp.controllers', [])
 
   }])
   .controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
-    var partiesRef = new Firebase('https://waitandeat2-randy.firebaseio.com/');
-
+    // Connect $scope.parties to live Firebase data.
+    var partiesRef = new Firebase('https://waitandeat2-randy.firebaseio.com/parties');
     $scope.parties = $firebase(partiesRef);
 
+    // Object to store data from waitlist form
     $scope.newParty = {name: '', phone: '', size: ''};
 
+    // Function to save a new party to waitlist
     $scope.saveParty = function() {
       $scope.parties.$add($scope.newParty);
       $scope.newParty = {name: '', phone: '', size: ''};
     };
+
+    // Function to send a text message to a party
+    $scope.sendTextMessage = function(phoneNumber) {
+      var textMessageRef = new Firebase('https://waitandeat2-randy.firebaseio.com/textMessages');
+      var textMessages = $firebase(textMessageRef);
+      textMessages.$add({phoneNumber: phoneNumber});
+    };
+
+
+
+
+
+
+
   }]);
   
